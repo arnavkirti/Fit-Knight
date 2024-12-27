05e1c91b0ad1ddf6a8b2e6a089a6d678b2d08088
Arnav Kirti <arnav.tkd@gmail.com>
2024-12-28 02:49:04 +0530

    feat: added routes for dashboard

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -1,8 +1,17 @@
-const express = require('express');
-const { adminSignup, adminLogin } = require('../controllers/admin');
+const express = require("express");
+const {
+  adminSignup,
+  adminLogin,
+  getGroupDetails,
+  getJoinRequests,
+  updateGroupDetails,
+} = require("../controllers/admin");
 const router = express.Router();
 
-router.post('/signup', adminSignup);
-router.post('/login', adminLogin);
+router.post("/signup", adminSignup);
+router.post("/login", adminLogin);
+router.get("/group-details", getGroupDetails);
+router.get("/join-requests", getJoinRequests);
+router.put("/update-group", updateGroupDetails);
 
 module.exports = router;
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,8 +1,15 @@
-const express = require('express');
-const { userSignup, userLogin } = require('../controllers/user');
+const express = require("express");
+const {
+  userSignup,
+  userLogin,
+  getRecommendedBuddies,
+  getAvailableGroups,
+} = require("../controllers/user");
 const router = express.Router();
 
-router.post('/signup', userSignup);
-router.post('/login', userLogin);
+router.post("/signup", userSignup);
+router.post("/login", userLogin);
+router.get("/recommended-buddies", getRecommendedBuddies);
+router.get("/available-groups", getAvailableGroups);
 
 module.exports = router;
